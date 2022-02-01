584a93d8faad74d315c34bcf849c0b2b71634e65
junnonaka <junnonaka6142@gmail.com>
2022-02-01 23:03:47 +0900

    first commit

---
+++ Banky2/Banky2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Banky2
+//
+//  Created by 野中淳 on 2022/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
